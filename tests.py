03e89665042d901bdc9d007f14a6d1ed8ceebf7b
elpekenin <58857054+elpekenin@users.noreply.github.com>
2022-01-28 23:54:58 +0100

    Delete tests.py

--- tests.py
+++
@@ -1,8 +0,0 @@
-from difflib  import get_close_matches
-from datos import info
-from pymongo import MongoClient
-database = MongoClient(info.database_ip)[info.database_name]
-
-cursor = dict(database["asignaturas"].find_one({"nombre": "Conceptos Avanzados de Internet"}, {"_id": False}))
-
-print("\n".join(k for k in cursor.values()))
